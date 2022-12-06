0064783accdbdcd4be60f7d4ce08ea72c5b2ec7f
Lengthrequired <hyp3rcr4ft@gmail.com>
2022-12-06 12:44:41 +0100

    footer (normalement il marche)

--- src/view/view.php
+++ src/view/view.php
@@ -21,13 +21,76 @@
                <a href="cart.php" class="cart">Cart</a>
             </nav>
       </header>
+      <wrap>
       <main>
-            <?php
-            require __DIR__ . "/{$cheminVueBody}";
-            ?>
+          <?php
+          require __DIR__ . "/{$cheminVueBody}";
+          ?>
       </main>
+      </wrap>
       <footer>
-        <p>Site de Covoiturage par <span>Florian Montourcy</span></p>
+          <div class="foot-container">
+              <div class="foot-left">
+
+                      <img src="../images/logo.png" alt="Logomini" class="logomini">
+
+
+              <div class="logo-Fuzzy">
+                  <h2>Fuzzy</h2>
+                  <ul>
+                        <li><a href="#">A propos</a></li>
+                        <li><a href="#">Contactez nous</a></li>
+              </div>
+                  </ul>
+              </div>
+                <div class="foot-leftcenter">
+                    <h2>Produits</h2>
+                    <ul>
+                        <li><a href="#"> Page Principale</a></li>
+                        <li><a href="#">Modele 3D</a></li>
+                        <li><a href="#">Accessoires</a></li>
+                        <li><a href="#">Projectiles</a></li>
+                    </ul>
+                </div>
+              <div class="foot-center">
+                    <h2>Support</h2>
+                    <ul>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Aide</a></li>
+                            <li><a href="#">Livraison</a></li>
+                    </ul>
+              </div>
+              <div class="foot-rightcenter">
+                    <h2>Mon compte</h2>
+                    <ul>
+                            <li><a href="#">Mon compte</a></li>
+                            <li><a href="#">Mes commandes</a></li>
+                            <li><a href="#">Mes retours</a></li>
+                            <li><a href="#">Mes informations</a></li>
+                    </ul>
+              </div>
+
+              <div class="foot-right-right">
+                  <h2>Ressources</h2>
+                    <ul>
+                        <li><a href="#">Feedbacks</a></li>
+                        <li><a href="#">Sécurité</a></li>
+                        <li><a href="#">Licence vente</a></li>
+              </div>
+              <div class="foot-right-right-right">
+                    <h2>Conditions</h2>
+                        <ul>
+                            <li><a href="#">CGV</a></li>
+                            <li><a href="#">CGU</a></li>
+                            <li><a href="#">Politique de confidentialité</a></li>
+                            <li><a href="#">Cookies</a></li>
+                            <li><a href="#">Retour</a></li>
+                            <li><a href="#">Livraison</a></li>
+              </div>
+            </div>
+          <div class="foot-bottom">
+              <p>© 2022 Fuzzy, Inc. All rights reserved.</p>
+            </div>
       </footer>
    </body>
 </html>
