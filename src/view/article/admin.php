b98c10fc90fcb91e3ebf0f91cf4c25aaa09b6a80
Lengthrequired <hyp3rcr4ft@gmail.com>
2023-01-03 23:02:49 +0100

    page admin avec DeletedAdmin

--- src/view/article/admin.php
+++ src/view/article/admin.php
@@ -16,16 +16,18 @@
 
                     /* Clickable cards */
                     echo "<a href='frontController.php?action=read&idArticle=" . $id . "'>";
-
+                    echo "<div class='pack'>";
                     echo "<div class='card'>";
                     echo "<img src=\"../images/".$a->getCheminImageTile()."\" alt = \"presentation\" class=\"cardImage\">";
                     echo "<p>Nom : " . $a->getNom() . "</p>";
                     echo "<p>Marque : " . $a->getMarque() . "</p>";
                     echo "<p>Prix : " . $a->getPrixBatk() . "</p>";
                     echo "<img src='../images/k.png' alt='Logo' class='coinIcon'>";
+                    echo "</div>";
+
+                    echo "</a>";
                     echo "<a href='frontController.php?action=delete&idArticle=" . $id . "'>Supprimer</a>";
                     echo "</div>";
-                    echo "</a>";
                 }
                 echo "</div>";
 
---
+++ src/view/article/deletedAdmin.php
@@ -0,0 +1,3 @@
+<?php
+require_once __DIR__ . '/../../view/article/admin.php';
+echo "L'article : " . $article->getNom() . " a été supprimé";
