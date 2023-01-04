24236cd95352bd7577b4e95c217ff10c4d8bb859
Florian <floflomontage@gmail.com>
2023-01-04 02:06:53 +0100

    Delete button

--- src/view/article/list.php
+++ src/view/article/list.php
@@ -14,6 +14,12 @@
             echo "<a href='frontController.php?action=read&idArticle=" . $id . "'>";
 
             echo "<div class='card'>";
+            // If admin is connected, display the delete button
+            if (isset($_SESSION['user']) && $_SESSION['user']->getAdmin()) {
+                echo "<a href='frontController.php?action=delete&idArticle=" . $id . "' class='deleteButton'>
+                    <img src='../images/delete.png' alt='Supprimer' class='deleteImage'>
+                </a>";
+            }
             echo "<img src=\"../images/".$a->getCheminImageTile()."\" alt = \"presentation\" class=\"cardImage\">";
             echo "<p>" . $a->getNom() . "</p>";
             echo "<p>" . $a->getPrixBatk() . " <img src='../images/k.png' alt='Logo' class='coinIcon'></p>";
