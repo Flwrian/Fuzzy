9895a4cda6296b3cce14d728cac2c2c01907e405
Flwrian <floflomontage@gmail.com>
2023-01-04 18:13:46 +0100

    rm voiture

--- src/view/voiture/created.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../../view/voiture/list.php';
-echo "<p>La voiture a bien été créée</p>";
-?>
--- src/view/voiture/deleted.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once __DIR__ . '/../../view/voiture/list.php';
-echo "La voiture d'immatriculation : " . $voiture->getImmatriculation() . " a bien été supprimée.";
