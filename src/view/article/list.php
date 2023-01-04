e03ff8a5bd52e23768800aa5fb56aea57f86e105
Florian <floflomontage@gmail.com>
2023-01-04 02:46:34 +0100

    cards

--- src/view/article/list.php
+++ src/view/article/list.php
@@ -11,18 +11,23 @@
             $id = rawurlencode($a->getId());
 
             /* Clickable cards */
-            echo "<a href='frontController.php?action=read&idArticle=" . $id . "'>";
 
-            echo "<div class='card'>";
             // If admin is connected, display the delete button
             if (isset($_SESSION['user']) && $_SESSION['user']->getAdmin()) {
                 echo "<a href='frontController.php?action=delete&idArticle=" . $id . "' class='deleteButton'>
                     <img src='../images/delete.png' alt='Supprimer' class='deleteImage'>
                 </a>";
             }
+            
+            echo "<a href='frontController.php?action=read&idArticle=" . $id . "'>";
+
+
+            echo "<div class='card'>";
             echo "<img src=\"../images/".$a->getCheminImageTile()."\" alt = \"presentation\" class=\"cardImage\">";
+            
             echo "<p>" . $a->getNom() . "</p>";
             echo "<p>" . $a->getPrixBatk() . " <img src='../images/k.png' alt='Logo' class='coinIcon'></p>";
+            
             echo "</div>";
             echo "</a>";
         }
